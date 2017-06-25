e28d73698a32e54c5b1738bc4e89ba31ab2ae179
cdriscol <drizkol@gmail.com>
2017-06-24 20:52:16 -0600

    Fix the margins!!

--- client/modules/landing/sections/Home.js
+++ client/modules/landing/sections/Home.js
@@ -10,13 +10,13 @@ class HomeSection extends Component {
       <div className={styles.home__wrapper}>
         <div className={styles.home__content}>
           <p className={styles.home__secondary}>Together with their families</p>
-          <h3 className={styles.home__primary}>Amanda Thompson</h3>
+          <h3 className={[styles.home__primary, styles.amanda].join(' ')}>Amanda Thompson</h3>
           <div className={styles.home__amp__wrapper}>
             <div className={styles.home__amp__line} />
             <div className={styles.home__amp}>&amp;</div>
             <div className={styles.home__amp__line} />
           </div>
-          <h3 className={styles.home__primary}>Erik Christensen</h3>
+          <h3 className={[styles.home__primary, styles.erik].join(' ')}>Erik Christensen</h3>
           <h3 className={styles.home__secondary}>Request the honor of your presence<br />at their wedding celebration</h3>
           <button className={styles.home__button} onClick={this.handleRsvpClick}>RSVP</button>
         </div>

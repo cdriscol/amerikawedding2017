797823fececaa4066b07065cb75b65ff29495868
cdriscol <drizkol@gmail.com>
2017-06-24 21:23:13 -0600

    Fixing min width on the form

--- client/modules/landing/sections/Rsvp.js
+++ client/modules/landing/sections/Rsvp.js
@@ -171,7 +171,7 @@ class RsvpSection extends Component {
           componentClass="textarea"
           type="textarea"
           label="Send a message to the couple"
-          placeholder="Write your message!"
+          placeholder="Write your message.."
           className={[styles.rsvp__form__input]}
           onChange={this.handleMessageChange}
           value={message}
@@ -203,7 +203,7 @@ class RsvpSection extends Component {
   renderHello = () => {
     const { row } = this.state;
     return (
-      <h4>Hello, {row.names}</h4>
+      <h4>Hello, {row.names}!</h4>
     );
   };
 
